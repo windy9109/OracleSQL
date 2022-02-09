035b644843fe738abf30dd7b9ed6a2d1b95af769
windy9109 <windy9109@naver.com>
2022-02-09 10:11:48 +0900

    211

--- 20220208-02.sql
+++ 20220208-02.sql
@@ -1,30 +1,30 @@
 2022-0208-02)
-  ** CART테이블에 다음자료를 저장하시오
-        구매일자: 오늘(20050728)
-        구매회원: d001
-        구매상품
+  ** CART?��?��블에 ?��?��?��료�?? ???��?��?��?��
+        구매?��?��: ?��?��(20050728)
+        구매?��?��: d001
+        구매?��?��
         -----------------------------
-        상품번호         수량
+        ?��?��번호         ?��?��
         -----------------------------
         P201000003      3
         P201000015      2
  
         
-   
+    
         
         
 
---자동으로 증가되어지는 번호 예제
---함수로 날짜에 해당된 CART번호를 만들어보자
+--?��?��?���? 증�??��?���??�� 번호 ?��?��
+--?��?���? ?��짜에 ?��?��?�� CART번호�? 만들?��보자
 
     CREATE OR REPLACE FUNCTION FN_CREATE_CARTNO(
         P_DATE DATE)
-        RETURN VARCHAR2 --반환타입
+        RETURN VARCHAR2 --반환???��
     IS
         V_CNO VARCHAR2(20):=TO_CHAR(P_DATE,'YYYYMMDD'); 
         V_FLAG NUMBER:=0;
     BEGIN
-        SELECT COUNT(*) INTO V_FLAG -- SUBSTR(CART_NO,1,8)=V_CNO;만족하는 값만큼 V_FLAG에 값을 넣는다
+        SELECT COUNT(*) INTO V_FLAG -- SUBSTR(CART_NO,1,8)=V_CNO;만족?��?�� 값만?�� V_FLAG?�� 값을 ?��?��?��
             FROM CART
           WHERE SUBSTR(CART_NO,1,8)=V_CNO;
           
@@ -41,7 +41,7 @@
     
     
     
-(실행)
+(?��?��)
     SELECT FN_CREATE_CARTNO(SYSDATE)
         FROM DUAL;
         
@@ -52,29 +52,29 @@
 
 
 
-    --구매시 동반되는 마일리지 증가는 트리거로 처리함
-    --트리거에서 처리해야할 내용: 재고 UPDATE, 마일리지 UPDATE
+    --구매?�� ?��반되?�� 마일리�? 증�??�� ?��리거�? 처리?��
+    --?��리거?��?�� 처리?��?��?�� ?��?��: ?���? UPDATE, 마일리�? UPDATE
     
     CREATE OR REPLACE TRIGGER TG_CART_CHANGE
       AFTER INSERT OR UPDATE OR DELETE ON CART
       FOR EACH ROW
     DECLARE
       V_QTY NUMBER:=0; --번호
-      V_PID PROD.PROD_ID%TYPE; --상품ID
-      V_MILE NUMBER:=0; --마일리지
-      V_DATE DATE; --날짜
-      V_MID MEMBER.MEM_ID%TYPE; --회원번호
+      V_PID PROD.PROD_ID%TYPE; --?��?��ID
+      V_MILE NUMBER:=0; --마일리�?
+      V_DATE DATE; --?���?
+      V_MID MEMBER.MEM_ID%TYPE; --?��?��번호
     BEGIN
       IF INSERTING THEN 
-        V_QTY:=:NEW.CART_QTY; --새로운 수량추가
-        V_PID:=:NEW.CART_PROD; --새로운 상품ID추가 
-        V_DATE:=TO_DATE(SUBSTR(:NEW.CART_NO,1,8)); --현재날짜추가
+        V_QTY:=:NEW.CART_QTY; --?��로운 ?��?��추�?
+        V_PID:=:NEW.CART_PROD; --?��로운 ?��?��ID추�? 
+        V_DATE:=TO_DATE(SUBSTR(:NEW.CART_NO,1,8)); --?��?��?��짜추�?
         V_MID:=:NEW.CART_MEMBER;
          SELECT V_QTY*PROD_MILEAGE INTO V_MILE
             FROM PROD
           WHERE PROD_ID=V_PID;
       ELSIF UPDATING THEN
-        V_QTY:=:NEW.CART_QTY - :OLD.CART_QTY; -- 남은수량 = 현재전체수량 - 기존수량
+        V_QTY:=:NEW.CART_QTY - :OLD.CART_QTY; -- ?��???��?�� = ?��?��?��체수?�� - 기존?��?��
         V_PID:=:NEW.CART_PROD;
         V_DATE:=TO_DATE(SUBSTR(:NEW.CART_NO,1,8));
         V_MID:=:NEW.CART_MEMBER;
@@ -106,7 +106,7 @@
     END;
 
 
-(재고조회)
+(?��고조?��)
 
           
           
@@ -128,7 +128,7 @@
     WHERE CART_NO='2005072800005'
      AND CART_PROD='P201000003';
 
-    --반품할경우
+    --반품?��경우
     DELETE FROM CART
      WHERE CART_NO='2005072800005'
        AND CART_PROD='P201000003';
