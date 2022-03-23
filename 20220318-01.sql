bcf8cfc6678e78db590ec0c191db1e777f137fbd
windy9109 <windy9109@naver.com>
2022-03-23 17:35:19 +0900

    22-03-23

--- 20220318-01.sql
+++ 20220318-01.sql
@@ -46,3 +46,28 @@ create table mymember(
 
 
 drop table mymember;
+
+
+
+
+create table jdbc_board(
+    board_no number not null,  -- 번호(자동증가)
+    board_title varchar2(100) not null, -- 제목
+    board_writer varchar2(50) not null, -- 작성자
+    board_date date not null,   	-- 작성날짜
+    board_cnt number default 0, -- 조회수
+    board_content clob,     	-- 내용
+    constraint pk_jdbc_board primary key (board_no)
+);
+
+
+
+create sequence board_seq
+    start with 1   -- 시작번호
+    increment by 1; -- 증가값
+    
+    
+    
+    
+    
+
