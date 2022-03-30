c97d3cdd2e8aeeea9df8744b09ea2b5e946afdf2
windy9109 <windy9109@naver.com>
2022-03-30 17:32:50 +0900

    0330

---
+++ 0330.sql
@@ -0,0 +1,8 @@
+select buyer_name, buyer_id from buyer;
+
+select * from buyer where buyer_id='P10101';
+
+
+select prod_id, prod_name from prod where prod_lgu = 'P102';
+
+select * from prod where prod_id='P102000001';
